ee44cbd97e3e84bda06afe7a760a9932033e6f24
lunnersword <lunnersword@163.com>
2020-07-09 15:19:58 +0800

    adjusted for swift 5

--- Package.swift
+++ Package.swift
@@ -3,14 +3,16 @@
 import PackageDescription
 
 let package = Package(
-	name: "PerfectTemplate",
+	name: "PerfectSessionMemory",
 	products: [
-		.executable(name: "PerfectTemplate", targets: ["PerfectTemplate"])
+		.executable(name: "PerfectSessionMemory", targets: ["PerfectSessionMemory"])
 	],
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", from: "3.0.0")
 	],
 	targets: [
-		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer"])
+		.target(name: "PerfectSessionMemory", dependencies: ["PerfectHTTPServer", "PerfectSession", "PerfectRequestLogger"])
 	]
 )
